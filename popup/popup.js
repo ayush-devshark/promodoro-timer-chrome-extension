d02f3caa87951ff760e294a62ea70f496796dae9
ayush-devshark <ayushagarwal.dev@gmail.com>
2021-08-06 19:22:04 +0530

    add task adding logic

--- popup/popup.js
+++ popup/popup.js
@@ -1,1 +1,30 @@
- 
+const startTimerBtn = document.getElementById('startBtn');
+const addTaskBtn = document.getElementById('addBtn');
+const taskContainer = document.getElementById('task-container');
+
+const tasks = [];
+
+const addTask = () => {
+    const taskNum = tasks.length;
+    tasks.push('');
+
+    const taskRow = document.createElement('div');
+    const taskTextEL = document.createElement('input');
+
+    taskTextEL.type = 'text';
+    taskTextEL.placeholder = 'Enter a task...';
+    taskTextEL.addEventListener('change', () => {
+        tasks[taskNum] = taskTextEL.value;
+    });
+
+    const deleteTaskBtn = document.createElement('input');
+    deleteTaskBtn.type = 'button';
+    deleteTaskBtn.value = 'x';
+
+    taskRow.appendChild(taskTextEL);
+    taskRow.appendChild(deleteTaskBtn);
+
+    taskContainer.appendChild(taskRow);
+};
+
+addTaskBtn.addEventListener('click', addTask);
